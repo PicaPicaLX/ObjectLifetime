fd53772c963322b75b5683c93f4ff1405af954ad
栗鑫 <lx389029980@outlook.com>
2018-02-05 00:27:52 +0800

    V1

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -11,6 +11,33 @@ namespace ObjectLifetime
         static void Main(string[] args)
         {
             Car myCar = new Car();
+
+            myCar.Make = "OldMobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+            // 注释里的的等效于上面的
+            //Car myOtherCar = myCar;
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myOtherCar.Make, 
+                myOtherCar.Model, 
+                myOtherCar.Year, 
+                myOtherCar.Color);
+            Console.ReadLine();
+
+            myOtherCar.Model = "98";
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+            Console.ReadLine();
+
+
         }
     }
     
