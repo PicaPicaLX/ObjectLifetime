e86c564388a3691d8de6285d798480290d7c61fb
栗鑫 <lx389029980@outlook.com>
2018-02-05 00:35:57 +0800

    V1

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -10,7 +10,7 @@ namespace ObjectLifetime
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car();
+            Car myCar = new Car(); // 在Car类中创建新的对象myCar
 
             myCar.Make = "OldMobile";
             myCar.Model = "Cutlas Supreme";
@@ -18,25 +18,25 @@ namespace ObjectLifetime
             myCar.Color = "Silver";
 
 
-            Car myOtherCar;
-            myOtherCar = myCar;
+            Car myOtherCar; // 在Car类中创建新对象myOtherCar
+            myOtherCar = myCar; // 这一步将内存中对myCar对象的引用复制到了myOtherCar的新变量中
             // 注释里的的等效于上面的
             //Car myOtherCar = myCar;
             Console.WriteLine("{0} {1} {2} {3}", 
                 myOtherCar.Make, 
                 myOtherCar.Model, 
                 myOtherCar.Year, 
-                myOtherCar.Color);
-            Console.ReadLine();
+                myOtherCar.Color); // 因为两个变量指向同一个值，因此无论输出哪一个，得到的是相同的
+            //Console.ReadLine();
 
-            myOtherCar.Model = "98";
+            myOtherCar.Model = "98"; // 这一步将Model改为98
             Console.WriteLine("{0} {1} {2} {3}", 
                 myCar.Make, 
                 myCar.Model, 
                 myCar.Year, 
-                myCar.Color);
+                myCar.Color); // 输出后Model发生了变化，是因为两个变量指向相同的值，无论修改哪一个，值也会改变
             Console.ReadLine();
-
+            // 上几步操作证明了同一个内存对象可以有两个引用，类似一个气球有两条线，牵任何一条都可以找到气球
 
         }
     }
