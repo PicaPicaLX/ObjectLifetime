b3d48e81b3fb42fed260d2a49570f39f9f83d057
栗鑫 <lx389029980@outlook.com>
2018-02-04 23:29:53 +0800

    V1

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -16,6 +16,9 @@ namespace ObjectLifetime
     
     class Car
     {
-
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
     }
 }
